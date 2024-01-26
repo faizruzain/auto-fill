cdd73b276c980fe8c7eb02be319f4e220b88fc99
Faiz Ruzain <faiz.faiz8383@gmail.com>
2024-01-26 18:25:15 +0700

    array-buffer

--- routes/send-me-img.js
+++ routes/send-me-img.js
@@ -1,27 +1,52 @@
 const express = require("express");
 const router = express.Router();
 const bot = require("../telegram-bot/bot");
+const multer = require("multer");
+
+const storage = multer.memoryStorage();
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "./evidence");
+//   },
+//   filename: (req, file, cb) => {
+//     const uniqueSuffix = Date.now();
+//     var extension = file.mimetype;
+//     extension = extension.split("/")[1];
+//     cb(null, req.body.ticketId + "-" + uniqueSuffix + "." + extension);
+//   },
+// });
+
+const upload = multer({ storage: storage });
+// const { unlink } = require("node:fs/promises");
 
 router
   .route("/")
   .get(async (req, res) => {
     try {
-      await bot.sendMessage("830056518", "url");
+      await bot.sendMessage("830056518", "hello form web!");
+      res.status(200).send({ message: "Telegram Bot" });
     } catch (error) {
       console.log("error");
       console.log(error);
+      res.status(400).send({ message: error });
     }
-    res.status(200).send({ message: "Telegram Bot" });
   })
-  .post(async (req, res) => {
-    console.log(req.body);
-    const urls = req.body.urls;
+  .post(upload.single("evidence"), async (req, res) => {
+    console.log(req.file);
+    console.log(req.body.ticketId);
+    const buffer = req.file.buffer;
+    // const path = req.file.path;
+    // res.status(200).send({ message: "Got your data" });
     try {
-      await bot.sendPhoto("830056518", urls[0]);
+      await bot.sendPhoto("830056518", buffer);
+      // await unlink(path);
+      // console.log(`successfully deleted ${path}`);
+      res.status(200).send({ message: "Got your data" });
     } catch (error) {
       console.log(error);
+      res.status(400).send({ message: "???" });
     }
-    res.status(200).send({ message: "Got your data" });
   });
 
 module.exports = router;
