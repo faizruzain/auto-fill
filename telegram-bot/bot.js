1ec709936d6203723d05778431edbe9f9b0e4588
Faiz Ruzain <faiz.faiz8383@gmail.com>
2024-01-27 10:11:37 +0700

    created new bot for production

--- telegram-bot/bot.js
+++ telegram-bot/bot.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const TelegramBot = require("node-telegram-bot-api");
 // replace the value below with the Telegram token you receive from @BotFather
-const token = process.env.TELEGRAM_BOT_TOKEN;
+const token = process.env.TELEGRAM_BOT_TOKEN_LOKAL || process.env.TELEGRAM_BOT_TOKEN_PRODUCTION;
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
 const chatId = process.env.FAIZRUZAIN || process.env.EOS_CSO_TELEGRAM_CHAT_ID;
