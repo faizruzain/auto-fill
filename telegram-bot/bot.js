d336d2ec814a437fb408dcc584b65276191b2602
Faiz Ruzain <faiz.faiz8383@gmail.com>
2024-01-31 11:33:21 +0700

    remove error logs

--- telegram-bot/bot.js
+++ telegram-bot/bot.js
@@ -14,8 +14,8 @@ bot.onText(/\/chatid/, (msg, match) => {
   bot.sendMessage(chatId, chatId);
 });
 
-bot.on("polling_error", (error) => {
-  console.log(error);
-});
+// bot.on("polling_error", (error) => {
+//   console.log(error);
+// });
 
 module.exports = bot;
